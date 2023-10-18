599c5de3c85a7cae4cd9163909f555e445782c2a
Rafael Ponte <rafael.ponte@zup.com.br>
2023-10-18 12:10:22 -0300

    computed inputs

--- templates/src/main/java/com/example/demo/{{project_name_capitalized}}Application.java
+++ templates/src/main/java/com/example/demo/{{project_name_capitalized}}Application.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package {{project_base_package}};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoAppApplication {
+public class {{project_name_capitalized}}Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAppApplication.class, args);
